ebeb2bd2501e5d8d92a3988b27a7bef39df28e14
Bigsam08 <uelsam994@gmail.com>
2023-02-21 07:36:14 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,6 +5,14 @@
  */
 int main(void)
 {
-	-putchar('_'; 'p'; 'u'; 't'; 'c'; 'h'; 'a'; 'r'; '/n');
+	_putchar('_');
+		_putchar('p');
+		_putchar('u');
+		_putchar('t');
+		_putchar('c');
+		_putchar('h');
+		_putchar('a');
+		_putchar('r');
+		_putchar('/n');
 	return (0);
 }
