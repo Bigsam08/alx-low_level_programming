27bb9fbc8123b27f6dc3b013f913f1850e0cf6f0
Bigsam08 <uelsam994@gmail.com>
2023-02-27 05:41:24 -0800

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,25 +1,22 @@
 #include "main.h"
 
 /**
- * rev_string - Entry point
+ * print_rev - Entry point
  * @s: pointer
  * Return: Always 0 (Success)
  */
 
-void rev_string(char *s)
+void print_rev(char *s)
 {
 	int i;
 	int j;
-	char m;
 
-	for (i = 0; s[i] != '\0'; i++)
+	for (i = 0 ; s[i] != '\0' ; i++)
 	{
 	}
-	i--;
-	for (j = 0; j < i; j++, i--)
+	for (j = i - 1 ; j >= 0 ; j--)
 	{
-		m = *(s + i);
-		*(s + i) = *(s + j);
-		*(s + j) = m;
+		_putchar(s[j]);
 	}
+	_putchar('\n');
 }
